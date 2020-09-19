757c888033c70f1e863ef510f7af79d67b134617
nicoletteheath <67173671+nicoletteheath@users.noreply.github.com>
2020-09-19 10:41:10 +0800

    Home and Project Navs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Nav from "./components/Nav/Nav";
+import Home from "./pages/HomePage";
+import ProjectPage from "./pages/ProjectPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+export default function App() {
+    return (    
+        <Router>
+            <div>
+                <Nav />
+                    <Switch>
+                        <Route path="/project">
+                            <ProjectPage />
+                        </Route>
+                        <Route path="/">
+                            <Home />
+                        </Route>
+                    </Switch>
+            </div>
+        </Router>
+    );
+}                  
