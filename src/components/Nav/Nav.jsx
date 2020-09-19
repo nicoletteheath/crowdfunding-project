ae80db4a4c844b1e86747c9cfac7d887a6ecd7d9
nicoletteheath <67173671+nicoletteheath@users.noreply.github.com>
2020-09-19 12:49:50 +0800

    fixed nav colour

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Nav.css";
 
 
 function Nav() {
     return (
-        <nav>
+        <nav className="nav">
             <Link to="/">Home</Link>
             <Link to="/project">Project</Link>
         </nav>
