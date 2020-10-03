d4c8518fd7f8789e6d85f827efa414fea0930675
nicoletteheath <67173671+nicoletteheath@users.noreply.github.com>
2020-10-03 09:54:48 +0800

    create project page complete

--- src/components/CreateProjectForm/CreateProjectForm.jsx
+++ src/components/CreateProjectForm/CreateProjectForm.jsx
@@ -4,12 +4,12 @@ import { useHistory } from "react-router-dom";
     function CreateProject() {
 
         const [credentials, setCredentials] = useState({
-            "title": "",
-            "description": "test description",
-            "goal": 150,
-            "image": "http://lorempixel.com/400/400/nightlife",
-            "is_open": true,
-            "date_created": "2020-09-04T11:33:37Z",
+            title: "",
+            description: "test description",
+            goal: 150,
+            image: "http://lorempixel.com/400/400/nightlife",
+            is_open: true,
+            date_created: "2020-09-04T11:33:37Z",
         });
         const history = useHistory();
         const token = window.localStorage.getItem("token");
@@ -28,14 +28,14 @@ import { useHistory } from "react-router-dom";
                 method: "post",
                 headers: {
                     "Content-Type": "application/json",
-                    "Authorization": `token ${token}`,
+                    Authorization: `token ${token}`,
                 },
                 body: JSON.stringify(credentials),
             }
             );
 
-            const data = await response.json()
-            window.localStorage.setItem("token", data.token)
+            // const data = await response.json()
+            // window.localStorage.setItem("token", data.token)
             
             return response.json();
         };
@@ -44,15 +44,10 @@ import { useHistory } from "react-router-dom";
     
         const handleSubmit = (e) => {
             e.preventDefault();
-            if (credentials.title && 
-                credentials.description && 
-                credentials.goal && 
-                // credentials.image && 
-                credentials.is_open && 
-                credentials.date_created
+            if (true
                 ) {
                 postData().then((response) => {
-                    window.localStorage.setItem("token", response.token);
+                    // window.localStorage.setItem("token", response.token);
                     history.push("/");
                 })
                 .catch((error) => {
@@ -65,27 +60,27 @@ import { useHistory } from "react-router-dom";
             <form>
                 <div>
                     <label htmlFor="title">Title:</label>
-                    <input type="text" id="titl" placeholder="Enter Title" onChange={handleChange}/>
+                    <input type="text" id="title" placeholder="Enter Title" onChange={handleChange}/>
                 </div>
                 <div>
                     <label htmlFor="description">Description:</label>
-                    <input type="description" id="description" placeholder="description" onChange={handleChange}/>
+                    <input type="description" id="description" placeholder="description" onChange={handleChange} value={credentials.description}/>
                 </div>
                 <div>
                     <label htmlFor="goal">Goal:</label>
-                    <input type="goal" id="goal" placeholder="Goal" onChange={handleChange}/>
+                    <input type="goal" id="goal" placeholder="Goal" onChange={handleChange} value={credentials.goal}/>
                 </div>
                 <div>
                     <label htmlFor="image">Image:</label>
-                    <input type="image" id="image" placeholder="Image" onChange={handleChange}/>
+                    <input type="image" id="image" placeholder="Image" onChange={handleChange} value={credentials.image}/>
                 </div>
                 <div>
                     <label htmlFor="is_open">Project Open:</label>
-                    <input type="is_open" id="is_open" placeholder="is_open" onChange={handleChange}/>
+                    <input type="is_open" id="is_open" placeholder="is_open" onChange={handleChange} value={credentials.is_open}/>
                 </div>
                 <div>
                     <label htmlFor="date_created">Date Created:</label>
-                    <input type="date_created" id="date_created" placeholder="date_Created" onChange={handleChange}/>
+                    <input type="date_created" id="date_created" placeholder="date_Created" onChange={handleChange} value={credentials.date_created}/>
                 </div>
                 <button type="submit" onClick={handleSubmit}>
                     Create Project
--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,60 +1,67 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 
-    function LoginForm() {
-
-        const [credentials, setCredentials] = useState({
-            username: "",
-            password: "",
+function LoginForm() {
+    const [credentials, setCredentials] = useState({
+      username: "",
+      password: "",
+    });
+    const history = useHistory();
+    const handleChange = (e) => {
+      const { id, value } = e.target;
+      setCredentials((prevCredentials) => ({
+        ...prevCredentials,
+        [id]: value,
+      }));
+    };
+    const postData = async () => {
+      const response = await fetch(
+        `${process.env.REACT_APP_API_URL}api-token-auth/`,
+        {
+          method: "post",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(credentials),
+        }
+      );
+      const data = await response.json();
+    //   window.localStorage.setItem("token", data.token);
+      return data;
+    };
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      if (credentials.username && credentials.password) {
+        postData().then((data) => {
+          window.localStorage.setItem("token", data.token);
+          history.push("/");
         });
-        const history = useHistory();
-
-        const handleChange = (e) => {
-            const { id, value } = e.target;
-            setCredentials((prevCredentials) => ({
-            ...prevCredentials,
-            [id]: value,
-            }));
-        };
-
-        const postData = async () => {
-            const response = await fetch(`${process.env.REACT_APP_API_URL}api-token-auth/`, {
-                method: "post",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(credentials),
-            }
-            );
-            return response.json();
-        };
-
-        
-        const handleSubmit = (e) => {
-            e.preventDefault();
-            if (credentials.username && credentials.password) {
-                postData().then((response) => {
-                    window.localStorage.setItem("token", response.token);
-                    history.push("/");
-            });
-            }
-        };
-
-        return (
-            <form>
-                <div>
-                    <label htmlFor="username">Username:</label>
-                    <input type="text" id="username" placeholder="Enter username" onChange={handleChange}/>
-                </div>
-                <div>
-                    <label htmlFor="password">Password:</label>
-                    <input type="password" id="password" placeholder="Password" onChange={handleChange}/>
-                </div>
-                <button type="submit" onClick={handleSubmit}>
-                    Login
-                </button>
-            </form>
-        );
-    }
-
-    export default LoginForm;
+      }
+    };
+    return (
+      <form>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            placeholder="Enter username"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Enter password"
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit" onClick={handleSubmit}>
+          Login
+        </button>
+      </form>
+    );
+  }
+  export default LoginForm;
